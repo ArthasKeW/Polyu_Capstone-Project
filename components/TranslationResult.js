82619b98bb5d161545b8dd9e59bbe14cba359ca6
Arthas Cheng <arthas@gmail.com>
2023-03-23 19:18:12 +0800

    new changes
    new changes

--- components/TranslationResult.js
+++ components/TranslationResult.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { StyleSheet, TouchableOpacity, View, Text } from "react-native"
 import { Entypo } from '@expo/vector-icons';
 import colors from "../utils/colors";
@@ -5,22 +6,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { useCallback } from "react";
 import { setSavedItems } from "../store/savedItemsSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Image } from 'react-native';
+import axios from 'axios';
+import * as Speech from 'expo-speech';
 
 export default TranslationResult = props => {
+    const searchPhotos = async (query) => {
+
+        try {
+            const response = await axios.get('https://api.unsplash.com/search/photos?page=1&query=' + query + '&client_id=QsjawRcx1O6uL1JZXT0cgNdGLqo95w8kZuj2i4gNnm0');
+            const photos = response.data.results.map((result) => result.urls.regular);
+            return photos;
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    };
     const dispatch = useDispatch();
 
-    const {itemId} = props;
+    const [expandedItems, setExpandedItems] = useState([]);
+    const { itemId } = props;
     const item = useSelector(state => {
         return state.history.items.find(item => item.id === itemId) || state.savedItems.items.find(item => item.id === itemId)
     });
     const savedItems = useSelector(state => state.savedItems.items);
-    const isSaved = savedItems.some(i => i.id===itemId);
+    const isSaved = savedItems.some(i => i.id === itemId);
     const starIcon = isSaved ? 'star' : 'star-outlined';
 
-    const starItem = useCallback(async()=>{
+    const starItem = useCallback(async () => {
         let newSavedItems;
 
-        if(isSaved) {
+        if (isSaved) {
             newSavedItems = savedItems.filter(i => i.id !== itemId);
         }
         else {
@@ -28,26 +44,118 @@ export default TranslationResult = props => {
             newSavedItems.push(item);
         }
 
-        await AsyncStorage.setItem('savedItems',JSON.stringify(newSavedItems));
+        await AsyncStorage.setItem('savedItems', JSON.stringify(newSavedItems));
 
-        dispatch(setSavedItems({items:newSavedItems}));
-    },[dispatch,savedItems]);
+        dispatch(setSavedItems({ items: newSavedItems }));
+    }, [dispatch, savedItems]);
 
-    return <View style={styles.container}>
-        <View style={styles.textContainer}>
-            <Text
-                numberOfLine ={4} 
-                style={styles.title}>{item.original_text}</Text>
-            <Text  
-                numberOfLine ={4}  
-                style={styles.subTitle}>{item.translated_text[item.to]}</Text>
+    const toggleExpand = async (itemId) => {
+        const expandedIndex = expandedItems.findIndex((item) => item.id === itemId);
+
+        if (expandedIndex >= 0) {
+            // Item is already expanded, so collapse it
+            setExpandedItems(expandedItems.filter((item) => item.id !== itemId));
+        } else {
+            // Item is not expanded, so expand it
+            const item = savedItems.find((item) => item.id === itemId);
+            const photos = await searchPhotos(item.original_text);
+
+            setExpandedItems([
+                ...expandedItems,
+                {
+                    id: itemId,
+                    photos,
+                    currentPhotoIndex: 0, // add currentPhotoIndex property
+                },
+            ]);
+        }
+    };
+    const expandedItem = expandedItems.find((i) => i.id === item.id);
+    const isExpanded = expandedItem !== undefined;
+
+    const speakOriginalText = () => {
+        Speech.speak(item.original_text, { language: item.from });
+    };
+
+    const speakTranslatedText = () => {
+        Speech.speak(item.translated_text[item.to], { language: item.to });
+    };
+
+    return <TouchableOpacity onPress={() => toggleExpand(item.id)}>
+        <View style={styles.container}>
+            <View style={styles.textContainer}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={speakOriginalText}>
+                        <Entypo name="controller-play" size={24} color={colors.subTextColor} />
+                    </TouchableOpacity>
+                    <Text
+                        numberOfLines={4}
+                        style={styles.title}>{item.original_text}</Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={speakTranslatedText}>
+                        <Entypo name="controller-play" size={24} color={colors.subTextColor} />
+                    </TouchableOpacity>
+                    <Text
+                        numberOfLines={4}
+                        style={styles.subTitle}>{item.translated_text[item.to]}</Text>
+                </View>
+            </View>
+            <TouchableOpacity style={styles.iconContainer} onPress={starItem}>
+                <Entypo name={starIcon} size={24} color={colors.subTextColor} />
+            </TouchableOpacity>
         </View>
-
-        <TouchableOpacity style={styles.iconContainer} onPress={starItem}>
-            <Entypo name={starIcon} size={24} color ={colors.subTextColor} />
-        </TouchableOpacity>
-    </View>
-
+        {isExpanded && (
+            <View style={styles.expandedContent}>
+                
+                <View style={styles.buttonContainer}>
+                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start' }}>
+                        <TouchableOpacity
+                            onPress={() =>
+                                setExpandedItems(
+                                    expandedItems.map((i) =>
+                                        i.id === item.id
+                                            ? {
+                                                ...i,
+                                                currentPhotoIndex:
+                                                    (i.currentPhotoIndex - 1 + i.photos.length) % i.photos.length, // go to previous photo
+                                            }
+                                            : i
+                                    )
+                                )
+                            }
+                        >
+                            <Text style={styles.buttonText}>{"<"}</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Image
+                    key={expandedItem.photos[expandedItem.currentPhotoIndex]}
+                    source={{ uri: expandedItem.photos[expandedItem.currentPhotoIndex] }}
+                    style={styles.photo}
+                />
+                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end' }}>
+                        <TouchableOpacity
+                            onPress={() =>
+                                setExpandedItems(
+                                    expandedItems.map((i) =>
+                                        i.id === item.id
+                                            ? {
+                                                ...i,
+                                                currentPhotoIndex:
+                                                    (i.currentPhotoIndex + 1) % i.photos.length, // go to next photo
+                                            }
+                                            : i
+                                    )
+                                )
+                            }
+                        >
+                            <Text style={styles.buttonText}>{">"}</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        )}
+    </TouchableOpacity>
 }
 
 const styles = StyleSheet.create({
@@ -58,26 +166,45 @@ const styles = StyleSheet.create({
         borderColor: colors.lightGrey,
         backgroundColor: 'white',
         borderWidth: 0.5,
-        borderTopWidth:0
+        borderTopWidth: 0
     },
     textContainer: {
-        flex:1,
+        flex: 1,
         marginRight: 8
     },
-    title:{
+    title: {
         fontFamily: 'nunito-bold',
         letterSpacing: 0.3,
         color: colors.textColor
     },
-    subTitle:{
+    subTitle: {
         fontFamily: 'nunito-regular',
         letterSpacing: 0.3,
         color: colors.subTextColor,
         fontSize: 13
     },
-    iconContainer:{
+    iconContainer: {
         width: 30,
         justifyContent: 'center',
         alignItems: 'center'
-    }
+    },
+    expandedContent: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: colors.white,
+    },
+    photo: {
+        width: 200,
+        height: 200,
+        resizeMode: 'cover',
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginVertical: 5,
+    },
+    buttonText: {
+        fontSize: 30,
+        fontWeight: "bold",
+    },
 })
--- screens/TranslatorScreen_Saved.js
+++ screens/TranslatorScreen_Saved.js
@@ -1,161 +1,47 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
 import TranslationResult from '../components/TranslationResult';
 import { globalStyles } from '../styles/global';
 import colors from '../utils/colors';
-import axios from 'axios';
-import {Image} from 'react-native' ; 
 
 export default function Home({ navigation }) {
-    const searchPhotos = async (query) => {
-
-        try {
-          const response = await axios.get('https://api.unsplash.com/search/photos?page=1&query='+query+'&client_id=QsjawRcx1O6uL1JZXT0cgNdGLqo95w8kZuj2i4gNnm0');
-          const photos = response.data.results.map((result) => result.urls.regular);
-          return photos;
-        } catch (error) {
-          console.error(error);
-          return [];
-        }
-      };
-
   const savedItems = useSelector(state => state.savedItems.items);
 
-  const [expandedItems, setExpandedItems] = useState([]);
-
-  const toggleExpand = async (itemId) => {
-    const expandedIndex = expandedItems.findIndex((item) => item.id === itemId);
-  
-    if (expandedIndex >= 0) {
-      // Item is already expanded, so collapse it
-      setExpandedItems(expandedItems.filter((item) => item.id !== itemId));
-    } else {
-      // Item is not expanded, so expand it
-      const item = savedItems.find((item) => item.id === itemId);
-      const photos = await searchPhotos(item.original_text);
-  
-      setExpandedItems([
-        ...expandedItems,
-        {
-          id: itemId,
-          photos,
-          currentPhotoIndex: 0, // add currentPhotoIndex property
-        },
-      ]);
-    }
-  };
-  
-
-  
-
-const renderItem = ({ item }) => {
-    const expandedItem = expandedItems.find((i) => i.id === item.id);
-    const isExpanded = expandedItem !== undefined;
-    
-    return (
-      <TouchableOpacity onPress={() => toggleExpand(item.id)}>
-        <TranslationResult itemId={item.id} />
-        {isExpanded && (
-          <View style={styles.expandedContent}>
-            <Image
-              key={expandedItem.photos[expandedItem.currentPhotoIndex]}
-              source={{ uri: expandedItem.photos[expandedItem.currentPhotoIndex] }}
-              style={styles.photo}
-            />
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                onPress={() =>
-                  setExpandedItems(
-                    expandedItems.map((i) =>
-                      i.id === item.id
-                        ? {
-                            ...i,
-                            currentPhotoIndex:
-                              (i.currentPhotoIndex - 1 + i.photos.length) % i.photos.length, // go to previous photo
-                          }
-                        : i
-                    )
-                  )
-                }
-              >
-                <Text style={styles.buttonText}>{"<"}</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() =>
-                  setExpandedItems(
-                    expandedItems.map((i) =>
-                      i.id === item.id
-                        ? {
-                            ...i,
-                            currentPhotoIndex:
-                              (i.currentPhotoIndex + 1) % i.photos.length, // go to next photo
-                          }
-                        : i
-                    )
-                  )
-                }
-              >
-                <Text style={styles.buttonText}>{">"}</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        )}
-      </TouchableOpacity>
-    );
-  };
-  
-
-  if (savedItems.length === 0) {
-    return (
-      <View style={styles.noItemsContainer}>
-        <Text style={styles.noItemText}>No Saved Items</Text>
+  if(savedItems.length ===0){
+      return <View style={styles.noItemsContainer}>
+          <Text style={styles.noItemText}>No Saved Items</Text>
       </View>
-    );
   }
 
   return (
     <View style={styles.container}>
-      <FlatList
+    <FlatList
         data={savedItems.slice().reverse()}
-        renderItem={renderItem}
-      />
-    </View>
-  );
-}
+        renderItem={itemData => {
+            return <TranslationResult itemId={itemData.item.id} />
+        }}
+    />
 
+</View>
+
+)
+}
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1,
-    backgroundColor: colors.greyBackground,
-    padding: 10,
+      flex: 1,
+      backgroundColor: colors.greyBackground,
+      padding: 10
   },
-  noItemsContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  noItemsContainer:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center'
   },
-  noItemText: {
-    fontFamily: 'nunito-regular',
-    letterSpacing: 0.3,
-  },
-  expandedContent: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    backgroundColor: colors.white,
-  },
-  photo: {
-    width: 200,
-    height: 200,
-    resizeMode: 'cover',
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginTop: 10,
-  },
-  buttonText: {
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-});
+  noItemText:{
+      fontFamily: 'nunito-regular',
+      letterSpacing:0.3
+  }
+
+})
